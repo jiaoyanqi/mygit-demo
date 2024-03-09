44e542a037ef4bc05e868dd634b4145be5769b6a
jiao <2086217792@qq.com>
2024-03-09 20:27:27 +0800

    commit first

--- src/main/java/com/soft/test/HelloWord.java
+++ src/main/java/com/soft/test/HelloWord.java
@@ -3,5 +3,7 @@ package com.soft.test;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("push test");
+        System.out.println("push test11111");
     }
 }
